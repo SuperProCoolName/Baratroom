20ca8dda0d6d5afa85108d6c2bf276e89d7a4596
SuperProCoolName <minzatov.2004@mail.ru>
2022-11-16 15:47:48 +0300

    fix 2/2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница стандартного бары */
+router.get('/standart', function(req, res, next) {
+  res.send("<h1>Страница standart</h1>")
+});
+
+/* Страница скина fury на бару*/
+router.get('/fury', function(req, res, next) {
+  res.send("<h1>Страница fury</h1>")
+});
+
+/* Страница бары из 1 доты*/
+router.get('/dota1', function(req, res, next) {
+  res.send("<h1>Страница dota1</h1>")
+});
+
 module.exports = router;
