b2a44f14832b88151237c5dd334ce1af1a08e5c8
SuperProCoolName <minzatov.2004@mail.ru>
2022-11-21 21:24:04 +0300

    added mongodb

---
+++ createDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("baratroom");
+database.dropDatabase()
+database = client.db("baratroom");
+const bara = database.collection("bara");
+const result = await bara.insertOne({name:"Подпивасик"});
+console.log(`bara ${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
