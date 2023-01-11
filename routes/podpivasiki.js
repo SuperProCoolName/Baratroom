56295ee87eaf3402a4cba1052884ba22e90850f0
SuperProCoolName <minzatov.2004@mail.ru>
2023-01-11 21:04:25 +0300

    8.3

--- routes/podpivasiki.js
+++ routes/podpivasiki.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET podpivasiki listing. */
+
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('new router for routes that start with "podpivasiki"');
+  res.send('New router for routes starting with podpivasiki');
 });
 
 module.exports = router;
 
+/* Page of baratrooms */
 router.get("/:nick", function(req, res, next) {
-  res.send(req.params.nick);
+    res.send(req.params.nick);
 });
+router.get('/:nick', function(req, res, next) {
+    Bara.findOne({nick:req.params.nick}, function(err,bara){
+        if(err) return next(err)
+        if(!bara) return next(new Error("There was no such baratroom, maybe you made a mistake in your request?"))
+        res.render('bara', {
+            title: bara.title,
+            picture: bara.avatar,
+            desc: bara.desc
+        })
+    })
+})
+
+
+module.exports = router;
