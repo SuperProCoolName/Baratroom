da94feef6141a929fd9ec3b183d8db3c63bc843d
SuperProCoolName <minzatov.2004@mail.ru>
2023-01-14 16:37:46 +0300

    9.1

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/baratroom')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/baratroom');
+var session = require('express-session');
 var podpivasiki = require('./routes/podpivasiki');
 
 
@@ -23,6 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "DotaBara",
+  cookie:{maxAge:60*1000},
+  resave: true,
+  saveUninitialized: true
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
