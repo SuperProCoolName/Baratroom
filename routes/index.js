79fedb894f40c09eb441c65a5badca683378249f
SuperProCoolName <minzatov.2004@mail.ru>
2023-01-14 15:32:06 +0300

    8.5

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Bara = require("../models/bara").Bara
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    Bara.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', {
+                                title: 'Express',
+                                menu: menu
+                            });
+    })
+
 });
-
-/* Standart baratroom 
-router.get('/standart', function(req, res, next) {
-  res.render("podpivas", {
-    title: "Standart Baratroom",
-    picture: "images/standart.jpg",
-    desc: "Standart Baratroom (Podpivas) from DotA 2"
-  });
-});*/
-
-/* Fury baratroom 
-router.get('/fury', function(req, res, next) {
-    res.render('podpivas', {
-    title: "Fury_Baratroom",
-    picture: "images/fury.jpg",
-    desc: "Skin 'Fury' Baratroom (Podpivas) from DotA 2"
-
-  });
-}); */
-
-/* DotA-AllStars baratroom
-router.get('/dota1', function(req, res, next) {
-  res.render("podpivas", {
-    title: "Old Baratroom",
-    picture: "images/dota1.jpg",
-    desc: "Old Baratroom (Podpivas) from DotA 1"
-  });
-}); */
 
 module.exports = router;
--- routes/podpivasiki.js
+++ routes/podpivasiki.js
@@ -1,30 +1,35 @@
-var express = require('express');
-var router = express.Router();
-var Bara = require('../models/bara').Bara
-
+var express = require('express')
+var router = express.Router()
+var Bara = require("../models/bara").Bara
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('New router for routes starting with podpivasiki');
+    res.send('New router for routes starting with podpivasiki')
 });
 
-module.exports = router;
-
-/* Page of baratrooms */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
+/* Страница котят */
 router.get('/:nick', function(req, res, next) {
-    Bara.findOne({nick:req.params.nick}, function(err,bara){
-        if(err) return next(err)
-        if(!bara) return next(new Error("There was no such baratroom, maybe you made a mistake in your request?"))
-        res.render('bara', {
-            title: bara.title,
-            picture: bara.avatar,
-            desc: bara.desc
+    async.parallel([
+            function(callback){
+                Bara.findOne({nick:req.params.nick}, callback)
+            },
+            function(callback){
+                Bara.find({},{_id:0,title:1,nick:1},callback)
+            }
+        ],
+        function(err,result){
+            if(err) return next(err)
+            var bara = result[0]
+            var baras = result[1] || []
+            if(!bara) return next(new Error("There was no such baratroom, maybe you made a mistake in your request?"))
+            res.render('bara', {
+                title: bara.title,
+                picture: bara.avatar,
+                desc: bara.desc,
+                menu: baras
+            });
         })
-    })
 })
-
 
 module.exports = router;
