f311b7dde0f612dd555f196cd564b908211f4315
SuperProCoolName <minzatov.2004@mail.ru>
2023-01-16 09:16:01 +0300

    10.4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Bara = require("../models/bara").Bara
-
+var User = require('../models/user').User
 
 
 
@@ -27,8 +27,24 @@ router.get('/logreg', function(req, res, next) {
 router.post('/logreg', function(req, res, next) {
     var username = req.body.username
     var password = req.body.password
-
-
+    User.findOne({username:username},function(err,user){
+        if(err) return next(err)
+        if(user){
+            if(user.checkPassword(password)){
+                req.session.user = user._id
+                res.redirect('/')
+            } else {
+                      res.render('logreg', {title: 'Log in'})
+            }
+        } else {
+            var user = new User({username:username,password:password})
+            user.save(function(err,user){
+                if(err) return next(err)
+                req.session.user = user._id
+                res.redirect('/')
+            })        
+      }
+    })
 });
 
 
