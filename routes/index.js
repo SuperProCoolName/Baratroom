f67f1cbd7839b51a472faced479da12417fb03cb
SuperProCoolName <minzatov.2004@mail.ru>
2023-01-16 09:13:22 +0300

    10.3

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router()
 var Bara = require("../models/bara").Bara
 
 
+
+
 router.get('/', function(req, res, next) {
     Bara.find({},{_id:0,title:1,nick:1},function(err,menu){
         req.session.greeting = "Session started"
@@ -17,7 +19,17 @@ router.get('/', function(req, res, next) {
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
     res.render('logreg',{title: 'Log in'});
-    });
     
+
+    });
+
+    /* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+
+
+});
+
 
 module.exports = router;
